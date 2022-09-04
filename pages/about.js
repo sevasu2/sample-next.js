e0dd180f74204c1a9ba937941f6c1833c8211f2d
sevas <babagame7@gmail.com>
2022-09-04 16:37:33 +0900

    add components

--- pages/about.js
+++ pages/about.js
@@ -1,39 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Hero from "../components/hero";
 
-function EachPost(props) {
-  return (
-    <>
-      <article>
-        <a href={props.url}>
-          <h3>{props.title}</h3>
-        </a>
-      </article>
-    </>
-  );
+export default function About() {
+  return <Hero title="About" subtitle="About development activities" />;
 }
-
-export default function Posts() {
-  return (
-    <>
-      <section>
-        <h2>おすすめ記事</h2>
-        <EachPost title="test1" url="/bolg/schedule" />
-        <EachPost title="test2" url="/bolg/schedule" />
-      </section>
-    </>
-  );
-}
-
-// export default function Home() {
-//   const test = (
-//     <main>
-//       <header>HEADER</header>
-//       <h1 style={{ color: "red", fontSize: "80px" }}>CUBE</h1>
-//       <p>アウトプットする</p>
-//       <footer>FOOTER</footer>
-//     </main>
-//   );
-//   return test;
-// }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
 import Header from "../components/header";
 import Hero from "../components/hero";
 import Footer from "../components/footer";
+import Layout from "../components/layout";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-
-      <main>
-        <Hero />
-      </main>
-
-      <Footer />
-    </>
-  );
+  return <Hero title="CUBE" subtitle="アウトプットしてください" />;
+  imageOn;
 }
