1ef348688824f59b7debb3d4f5f150fb5203b12c
Sevas <42911271+sevasu2@users.noreply.github.com>
2022-09-25 17:25:31 +0900

    test (#1)
    * test
    
    * fix test
    
    * add styles and components

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,12 @@ import Header from "../components/header";
 import Hero from "../components/hero";
 import Footer from "../components/footer";
 import Layout from "../components/layout";
+import Container from "../components/container";
 
 export default function Home() {
-  return <Hero title="CUBE" subtitle="アウトプットしてください" />;
-  imageOn;
+  return (
+    <Container>
+      <Hero title="CUBE" subtitle="アウトプットしてください" imageOn />
+    </Container>
+  );
 }
