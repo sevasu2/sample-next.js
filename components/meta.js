41ae56f4e512c550b076f07c7c80b92bfc6776ca
Sevas <42911271+sevasu2@users.noreply.github.com>
2022-10-10 17:11:51 +0900

    add meta tag (#3)

---
+++ components/meta.js
@@ -0,0 +1,43 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { siteMeta } from "../lib/constants";
+const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } =
+  siteMeta;
+
+export default function Meta({
+  pageTitle,
+  pageDesc,
+  pageImg,
+  pageImgW,
+  pageImgH,
+}) {
+  // ページのタイトル
+  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle;
+
+  // ページの説明
+  const desc = pageDesc || siteDesc;
+
+  // ページのURL
+  const router = useRouter();
+  const url = `${siteUrl}${router.asPath}`;
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+
+      <meta name="description" content={desc} />
+      <meta property="og:description" content={desc} />
+
+      <link rel="canonical" href={url} />
+      <meta property="og:url" content={url} />
+
+      <meta property="og:site_name" content={siteTitle} />
+      <meta property="og:type" content={siteType} />
+      <meta property="og:locale" content={siteLocale} />
+
+      <link rel="icon" href={siteIcon} />
+      <link rel="apple-touch-icon" href={siteIcon} />
+    </Head>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,12 @@ import Hero from "../components/hero";
 import Footer from "../components/footer";
 import Layout from "../components/layout";
 import Container from "../components/container";
+import Meta from "../components/meta";
 
 export default function Home() {
   return (
     <Container>
+      <Meta />
       <Hero title="CUBE" subtitle="アウトプットしてください" imageOn />
     </Container>
   );
